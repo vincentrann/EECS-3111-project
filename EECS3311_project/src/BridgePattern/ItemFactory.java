5c84eeffe40bb4a1d20bfa7c776af5d29ee07c7c
Jiho Han <96950003+yumsso@users.noreply.github.com>
2024-03-19 15:10:57 -0400

    Update ItemFactory.java

--- EECS3311_project/src/BridgePattern/ItemFactory.java
+++ EECS3311_project/src/BridgePattern/ItemFactory.java
@@ -2,13 +2,13 @@ package BridgePattern;
 
 public class ItemFactory {
     
-    public ClientItem (int uniqueID, String name, String typeOfItem,int copies, String library, boolean canBe) {
+    public ClientItem getVirtualItem(int uniqueID, String name, String typeOfItem,int copies, String library, boolean canBe) {
         return new ClientItem (new ConcretePhysicalItem(uniqueID, name, typeOfItem, copies, library, canBe));
     }
 
     
 
-    public ClientItem getItem(int uniqueID, String name, String typeOfItem,String edition, boolean availability, double priority ){
+    public ClientItem getPhsyicalItem(int uniqueID, String name, String typeOfItem,String edition, boolean availability, double priority ){
         return  new ClientItem(new ConcreteVirtualTextbook(uniqueID, name, typeOfItem, edition, availability, priority));
     }
 
